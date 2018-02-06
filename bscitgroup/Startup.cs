f74c9e9e7454b41e48f842ad3971afe23d3f2074
Sebastian Wilde Alarcon Arenas <sebastian.alarcon@ucsp.edu.pe>
2018-02-06 12:47:14 -0500

    Modelo y CRUD de Persona, modelo Categoria

--- bscitgroup/Startup.cs
+++ bscitgroup/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.EntityFrameworkCore;
+using bscitgroup.Models;
 
 namespace bscitgroup
 {
@@ -22,6 +24,9 @@ namespace bscitgroup
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddMvc();
+
+            services.AddDbContext<bscitgroupContext>(options =>
+                    options.UseSqlServer(Configuration.GetConnectionString("bscitgroupContext")));
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
